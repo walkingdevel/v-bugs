94ebf984bbbe2bc616286054f757889ec6f5c5d4
walkingdevel <mosquito.is.dead@gmail.com>
2022-12-13 13:13:25 +0000

    clojure: add prints

--- clojure/clojure.v
+++ clojure/clojure.v
@@ -14,7 +14,7 @@ mut:
 
 fn main() {
 	mut app := &App{
-		window: 0
+		window: &Window(0)
 	}
 
 	app.window = &Window{
@@ -22,17 +22,25 @@ fn main() {
 		on_click: app.method2
 	}
 
+	println('${@METHOD} | app: ${ptr_str(app)} | app.window: ${ptr_str(app.window)}')
+
 	app.window.on_resize()
 	app.window.on_click()
 }
 
 fn (mut app App) method1() {
+	println('${@METHOD} | app: ${ptr_str(app)} | app.window: ${ptr_str(app.window)}')
+
 	assert app.window != unsafe { nil }
 
 	println('method1() ✅')
 }
 
 fn (app App) method2() {
+	mut p := unsafe { &app }
+
+	println('${@METHOD} | &app: ${ptr_str(p)} | app.window: ${ptr_str(app.window)}')
+
 	assert app.window != unsafe { nil }
 
 	println('method2() ✅')
